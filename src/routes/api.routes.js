3ecd299a6392856ac6c12f87dffc73c817555a98
Hari Kishore <151401651+atomiclifestyle@users.noreply.github.com>
2025-07-17 06:30:15 +0000

    centraldb integration

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -7,15 +7,21 @@ import {
 } from '../controllers/controller.js';
 import { createUserDB, executeUserQuery } from '../controllers/db.controller.js';
 import checkUserId from '../middlewares/check.user_id.js';
+import { executeQuery } from '../controllers/central.db.controller.js';
 
 const router = express.Router();
 
+// Bhuvan API
 router.get('/routing', checkUserId, getRouting);
 router.get('/thematic', checkUserId, getThematicData);
 router.get('/vg', checkUserId, villageGeocoding);
 router.get('/ellipsoid', checkUserId, getEllipsoid);
 
+// Individual DB
 router.post('/create-user-db', checkUserId, createUserDB); 
 router.post('/execute-query', checkUserId, executeUserQuery); 
+
+// Central DB
+router.post('/execute-central-query', checkUserId, executeQuery);
 
 export default router;
